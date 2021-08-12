daba28e6469af84429582659afc0d4f791f0c5e6
Tom <tsfenwick@gmail.com>
2021-08-12 09:35:42 -0700

    uncomment out test

--- client/client_test.go
+++ client/client_test.go
@@ -2,18 +2,26 @@ package client
 
 import (
 	// "os"
+	"log"
+	"os"
 	"testing"
+
+	"github.com/joho/godotenv"
 )
 
 func Test_getAccessToken(t *testing.T) {
-	// testhost := os.Getenv("BANYAN_HOST")
-	// testRefreshToken := os.Getenv("BANYAN_REFRESH_TOKEN")
-	// client, err := New(testhost, testRefreshToken)
-	// if err != nil {
-	// 	t.Fatalf("%+v", err)
-	// }
-	// _, err = client.GetOidcSettings()
-	// if err != nil {
-	// 	t.Fatalf("%+v", err)
-	// }
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	testhost := os.Getenv("BANYAN_HOST")
+	testRefreshToken := os.Getenv("BANYAN_REFRESH_TOKEN")
+	client, err := New(testhost, testRefreshToken)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	_, err = client.GetOidcSettings()
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
 }
