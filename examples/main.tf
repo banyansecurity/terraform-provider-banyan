ccd966350dc81840c7bcf44e1ec44114770eb86d
TSFenwick <tom@banyansecurity.io>
2021-09-23 17:32:52 -0700

    set state correctly for reads (#20)

--- examples/main.tf
+++ examples/main.tf
@@ -57,216 +57,217 @@ provider "banyan" {
 //    group_id = data.okta_everyone_group.everyone.id
 //}
 
-resource "banyan_service" "test-service" {
-  cluster = "dev05-banyan"
-  name = "realtftest"
-  description = "description2"
-  metadatatags {
-    domain = "test2.com"
-    port = 1111
-    protocol = "https"
-    service_app_type = "WEB"
-    user_facing = false
-    template= "WEB_USER"
-    app_listen_port = 9191
-    include_domains = ["test.com"]
-  }
-  spec {
-    client_cidrs {
-      clusters = ["cliuster1&&&"]
-      address {
-        cidr = "127.127.127.1/32"
-        ports = "888"
-      }
-      address {
-        cidr = "255.220.225.1/32"
-        ports = "111"
-      }
-      host_tag_selectors {
-        host_tag_selector ={
-            "map" = "hi"
-          }
-        }
-      host_tag_selectors {
-        host_tag_selector ={
-          "map" = "hi"
-        }
-     }
-    }
-    attributes {
-      frontend_address {
-        cidr = "127.44.111.14/32"
-        port = "1111"
-      }
-      frontend_address {
-        cidr = "127.99.114.14/32"
-        port = 0
-      }
-      host_tag_selector {
-        site_name = "sitename"
-      }
-      host_tag_selector {
-        site_name = "sites"
-      }
-      tls_sni = ["sni_new2", "tls_sni616"]
-    }
-    backend {
-      target {
-        client_certificate = true
-        name = "targetbacknd"
-        port = 1515
-        tls = true
-        tls_insecure = true
-      }
-      dns_overrides = {
-        "internal.mysvc.com" = "10.23.0.1"
-        "exposed.service.com" : "internal.myservice.com"
-      }
-      backend_allowlist = ["allowme.com", "newurl.org"]
-      http_connect = true
-      connector_name = "hahah-connector-name"
-      backend_allow_pattern {
-        hostnames = ["backendallowhostName1", "hostname.com"]
-        cidrs = ["99.99.99.99/9"]
-        ports {
-          port_list = [111,222]
-          port_range {
-            min = 1
-            max = 5
-          }
-        }
-      }
-      backend_allow_pattern {
-        hostnames = ["differentone.com", "foo.bar.baz"]
-        cidrs = ["55.55.55.55/5"]
-        ports {
-          port_list = [88,99]
-          port_range {
-            min = 8
-            max = 9
-          }
-        }
-      }
-    }
-    cert_settings {
-      letsencrypt = false
-      dns_names = ["hello_dns_name", "dns_name2"]
-      custom_tls_cert {
-        enabled = false
-        cert_file = "asdf"
-        key_file = "asdf"
-      }
-    }
-    http_settings {
-      enabled = true
-
-      http_health_check {
-        enabled = true
-        addresses = ["88.99.101.2"]
-        method = "GET"
-        path = "/path"
-        user_agent = "chrome"
-        from_address = ["11.11.11.99"]
-        https = true
-      }
-      http_redirect {
-        enabled = true
-        addresses = ["127.98.2.1"]
-        from_address = ["43.12.31.1"]
-        url = "hello.com"
-        status_code = 209
-      }
-      oidc_settings {
-        enabled = true
-        service_domain_name = "service.domain.name"
-        post_auth_redirect_path = "/new/path"
-        api_path = "/api/path"
-        suppress_device_trust_verification = true
-        trust_callbacks = {
-          "h" = "y"
-          "b" = "j"
-        }
-      }
-      headers = {
-        "header1" = "headers"
-      }
-      exempted_paths {
-        enabled = true
-        paths = ["/path1", "/path2"]
-        pattern {
-          source_cidrs = ["222.222.222.222/8"]
-          methods = ["GET"]
-          paths = ["/path9000"]
-          mandatory_headers = ["mandatory_header"]
-          hosts {
-            origin_header = [
-              "https://originheader.org:80"]
-            target = [
-              "http://target.io:70"]
-          }
-        }
-        pattern {
-          source_cidrs = ["111.111.111.111/8"]
-          methods = ["POST"]
-          paths = ["/newPath"]
-          mandatory_headers = ["other_header"]
-          hosts {
-            origin_header = [
-              "http://other_originheader.com:90"]
-            target = [
-              "http://other_target.net:8080"]
-          }
-        }
-      }
-    }
-  }
-}
-//
-//resource "banyan_policy" "test-policy" {
-//  name = "realtfpolicytest"
-//  description = "realdescription"
+//resource "banyan_service" "test-service" {
+//  cluster = "dev05-banyan"
+//  name = "realtftest"
+//  description = "description2"
 //  metadatatags {
-//    template = "USER"
+//    domain = "test2.com"
+//    port = 1111
+//    protocol = "https"
+//    service_app_type = "WEB"
+//    user_facing = false
+//    template= "WEB_USER"
+//    app_listen_port = 9191
+//    include_domains = ["test.com"]
 //  }
 //  spec {
-//    access {
-//      roles = ["ANY", "HI"]
-//      rules {
-//        conditions {
-//          trust_level = "High"
+//    client_cidrs {
+//      clusters = ["cliuster1&&&"]
+//      address {
+//        cidr = "127.127.127.1/32"
+//        ports = "888"
+//      }
+//      address {
+//        cidr = "255.220.225.1/32"
+//        ports = "111"
+//      }
+//      host_tag_selectors {
+//        host_tag_selector ={
+//            "map" = "hi"
+//          }
 //        }
-//        l7_access {
-//          resources = ["*", "endpoint"]
-//          actions = ["*"]
+//      host_tag_selectors {
+//        host_tag_selector ={
+//          "map" = "hi"
 //        }
-//        l7_access {
-//          resources = ["*", "number2"]
-//          actions = ["*", "write"]
+//     }
+//    }
+//    attributes {
+//      frontend_address {
+//        cidr = "127.44.111.14/32"
+//        port = "1111"
+//      }
+//      frontend_address {
+//        cidr = "127.99.114.14/32"
+//        port = 0
+//      }
+//      host_tag_selector {
+//        site_name = "sitename"
+//      }
+//      host_tag_selector {
+//        site_name = "sites"
+//      }
+//      tls_sni = ["sni_new2", "tls_sni616"]
+//    }
+//    backend {
+//      target {
+//        client_certificate = true
+//        name = "targetbacknd"
+//        port = 1515
+//        tls = true
+//        tls_insecure = true
+//      }
+//      dns_overrides = {
+//        "internal.mysvc.com" = "10.23.0.1"
+//        "exposed.service.com" : "internal.myservice.com"
+//      }
+//      backend_allowlist = ["allowme.com", "newurl.org"]
+//      http_connect = true
+//      connector_name = "hahah-connector-name"
+//      backend_allow_pattern {
+//        hostnames = ["backendallowhostName1", "hostname.com"]
+//        cidrs = ["99.99.99.99/9"]
+//        ports {
+//          port_list = [111,222]
+//          port_range {
+//            min = 1
+//            max = 5
+//          }
+//        }
+//      }
+//      backend_allow_pattern {
+//        hostnames = ["differentone.com", "foo.bar.baz"]
+//        cidrs = ["55.55.55.55/5"]
+//        ports {
+//          port_list = [88,99]
+//          port_range {
+//            min = 8
+//            max = 9
+//          }
 //        }
 //      }
 //    }
-//    access {
-//      roles = [banyan_role.test-role.name]
-//      rules {
-//        conditions {
-//          trust_level = "Low"
-//        }
-//        l7_access {
-//          resources = ["*", "endpoint"]
-//          actions = ["*"]
-//        }
+//    cert_settings {
+//      letsencrypt = false
+//      dns_names = ["hello_dns_name", "dns_name2"]
+//      custom_tls_cert {
+//        enabled = false
+//        cert_file = "asdf"
+//        key_file = "asdf"
 //      }
 //    }
-//    exception {
-//      src_addr = ["127.0.0.1/32"]
-//    }
-//    options {
-//      disable_tls_client_authentication = true
-//      l7_protocol = "http"
+//    http_settings {
+//      enabled = true
+//
+//      http_health_check {
+//        enabled = true
+//        addresses = ["88.99.101.2"]
+//        method = "GET"
+//        path = "/path"
+//        user_agent = "chrome"
+//        from_address = ["11.11.11.99"]
+//        https = true
+//      }
+//      http_redirect {
+//        enabled = true
+//        addresses = ["127.98.2.1"]
+//        from_address = ["43.12.31.1"]
+//        url = "hello.com"
+//        status_code = 209
+//      }
+//      oidc_settings {
+//        enabled = true
+//        service_domain_name = "service.domain.name"
+//        post_auth_redirect_path = "/new/path"
+//        api_path = "/api/path"
+//        suppress_device_trust_verification = true
+//        trust_callbacks = {
+//          "h" = "y"
+//          "b" = "j"
+//        }
+//      }
+//      headers = {
+//        "header1" = "headers"
+//      }
+//      exempted_paths {
+//        enabled = true
+//        paths = ["/path1", "/path2"]
+//        pattern {
+//          source_cidrs = ["222.222.222.222/8"]
+//          methods = ["GET"]
+//          paths = ["/path9000"]
+//          mandatory_headers = ["mandatory_header"]
+//          hosts {
+//            origin_header = [
+//              "https://originheader.org:80"]
+//            target = [
+//              "http://target.io:70"]
+//          }
+//        }
+//        pattern {
+//          source_cidrs = ["111.111.111.111/8"]
+//          methods = ["POST"]
+//          paths = ["/newPath"]
+//          mandatory_headers = ["other_header"]
+//          hosts {
+//            origin_header = [
+//              "http://other_originheader.com:90"]
+//            target = [
+//              "http://other_target.net:8080"]
+//          }
+//        }
+//      }
 //    }
 //  }
 //}
-//
+
+resource "banyan_policy" "test-policy" {
+  name = "realtfpolicytest"
+  description = "realdescription"
+  metadatatags {
+    template = "USER"
+  }
+  spec {
+    access {
+      roles = ["ANY", "HI"]
+      rules {
+        conditions {
+          trust_level = "High"
+        }
+        l7_access {
+          resources = ["*", "endpoint"]
+          actions = ["*"]
+        }
+        l7_access {
+          resources = ["*", "number2"]
+          actions = ["*", "write"]
+        }
+      }
+    }
+    access {
+//      roles = [banyan_role.test-role.name]
+      roles = ["roleeee"]
+      rules {
+        conditions {
+          trust_level = "Low"
+        }
+        l7_access {
+          resources = ["*", "endpoint"]
+          actions = ["*"]
+        }
+      }
+    }
+    exception {
+      src_addr = ["127.0.0.1/32"]
+    }
+    options {
+      disable_tls_client_authentication = true
+      l7_protocol = "http"
+    }
+  }
+}
+
 //resource "banyan_role" "test-role" {
 // name = "realtfpolicytest"
 //  description = "realdescription"
