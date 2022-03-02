86402cf533533017feb4f9f626ce45169dd52c8c
Matt Schiller <mschiller@banyansecurity.io>
2022-03-02 11:00:37 -0800

    add local vars

---
+++ examples/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  project_id       = "terraform-dev-330921"
+  region           = "us-west1"
+  banyan_site_name = "mattorg"
+  service_name = "example-service"
+  zone = "us-west1-a"
+  network_name = "us-west1"
+  subnetwork_name = "us-west1-internal"
+  backend_port = 443
+  frontend_port = 443
+}
