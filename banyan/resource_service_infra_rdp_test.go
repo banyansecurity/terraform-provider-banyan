0b0b36d8f2ab83c3dff261d2dea8cf186e8646c9
Matt Schiller <mschiller@banyansecurity.io>
2022-04-07 10:09:43 -0700

    pass json comparison

--- banyan/resource_service_infra_rdp_test.go
+++ banyan/resource_service_infra_rdp_test.go
@@ -19,6 +19,7 @@ func TestAccService_rdp(t *testing.T) {
 				Config: testAccService_rdp_create(rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckExistingService("banyan_service_infra_rdp.acctest-rdp", &bnnService),
+					testAccCheckAgainstJson(t, testAccService_rdp_create_json(rName), &bnnService.ServiceID),
 				),
 			},
 		},
@@ -29,15 +30,85 @@ func TestAccService_rdp(t *testing.T) {
 func testAccService_rdp_create(name string) string {
 	return fmt.Sprintf(`
 resource "banyan_service_infra_rdp" "acctest-rdp" {
-  name        = "%s"
+  name        = "%s-rdp"
   description = "some RDP service description"
   cluster      = "us-west"
   access_tiers   = ["us-west1"]
   user_facing = true
-  domain      = "%s.corp.com"
-  backend {
-	  domain = "%s.internal"
-  }
+  domain      = "%s-rdp.corp.com"
+  backend_domain = "%s-rdp.internal"
+  backend_port = 3389
 }
 `, name, name, name)
+}
+
+func testAccService_rdp_create_json(name string) string {
+	return fmt.Sprintf(`
+{
+    "kind": "BanyanService",
+    "apiVersion": "rbac.banyanops.com/v1",
+    "type": "origin",
+    "metadata": {
+        "name": "%s-rdp",
+        "description": "some RDP service description",
+        "cluster": "us-west",
+        "tags": {
+            "template": "TCP_USER",
+            "user_facing": "true",
+            "protocol": "tcp",
+            "domain": "%s-rdp.corp.com",
+            "port": "8443",
+            "icon": "",
+            "service_app_type": "RDP",
+            "banyanproxy_mode": "TCP",
+            "app_listen_port": "3389",
+            "allow_user_override": false,
+            "description_link": ""
+        }
+    },
+    "spec": {
+        "attributes": {
+            "tls_sni": [
+                "%s-rdp.corp.com"
+            ],
+            "frontend_addresses": [
+                {
+                    "cidr": "",
+                    "port": "8443"
+                }
+            ],
+            "host_tag_selector": [
+                {
+                    "com.banyanops.hosttag.site_name": "us-west1"
+                }
+            ],
+            "disable_private_dns": false
+        },
+        "backend": {
+            "target": {
+                "name": "%s-rdp.internal",
+                "port": "3389",
+                "tls": false,
+                "tls_insecure": false,
+                "client_certificate": false
+            },
+            "dns_overrides": {},
+            "whitelist": [],
+            "connector_name": ""
+        },
+        "cert_settings": {
+            "dns_names": [
+                "%s-rdp.corp.com"
+            ],
+            "custom_tls_cert": {
+                "enabled": false,
+                "cert_file": "",
+                "key_file": ""
+            },
+            "letsencrypt": false
+        },
+        "client_cidrs": []
+    }
+}
+`, name, name, name, name, name)
 }
