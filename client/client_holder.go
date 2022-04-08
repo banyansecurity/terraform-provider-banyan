bb00857689bb0133ec869a592dcd54e16099d9eb
Matt Schiller <mschiller@banyansecurity.io>
2022-04-08 13:43:53 -0700

    wip

--- client/client_holder.go
+++ client/client_holder.go
@@ -6,6 +6,7 @@ import (
 	"github.com/banyansecurity/terraform-banyan-provider/client/policyattachment"
 	"github.com/banyansecurity/terraform-banyan-provider/client/restclient"
 	"github.com/banyansecurity/terraform-banyan-provider/client/role"
+	"github.com/banyansecurity/terraform-banyan-provider/client/satellite"
 	service "github.com/banyansecurity/terraform-banyan-provider/client/service"
 )
 
@@ -15,6 +16,7 @@ type ClientHolder struct {
 	Role             role.RoleClienter
 	PolicyAttachment policyattachment.Clienter
 	Admin            *admin.Admin
+	Satellite        satellite.SatelliteClienter
 }
 
 // NewClientHolder returns a new client which is used to perform CRUD operations on all Banyan resources.
@@ -30,6 +32,7 @@ func NewClientHolder(hostUrl string, refreshToken string) (client *ClientHolder,
 	client.Policy = policy.NewClient(restClient)
 	client.Role = role.NewClient(restClient)
 	client.PolicyAttachment = policyattachment.NewClient(restClient)
+	client.Satellite = satellite.NewClient(restClient)
 	admin := admin.NewClient(restClient)
 	client.Admin = admin
 	return
