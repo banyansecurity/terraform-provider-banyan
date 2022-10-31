f381124192d29d5a9030c9278eacb949b03d275c
Matt Schiller <mschiller@banyansecurity.io>
2022-10-31 16:47:38 -0700

    update get

--- client/shield/client.go
+++ client/shield/client.go
@@ -2,12 +2,11 @@ package shield
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/banyansecurity/terraform-banyan-provider/client/restclient"
 	"log"
+	"net/url"
 )
-
-const apiVersion = "api/v2"
-const component = "shield_config"
 
 type Shield struct {
 	Clusters []Info
@@ -42,13 +41,25 @@ type Clienter interface {
 
 func (c *Client) GetAll() (shields []string, err error) {
 	log.Printf("getting shields")
-	resp, err := c.restClient.Read(apiVersion, component, "", "")
-	var j ResponseBody
-	err = json.Unmarshal(resp, &j)
+	path := fmt.Sprintf("api/v2/shield_config")
+	myUrl, err := url.Parse(path)
 	if err != nil {
 		return
 	}
-	sInfo := j.Data.Configs
+	response, err := c.restClient.DoGet(myUrl.String())
+	if err != nil {
+		return
+	}
+	responseData, err := restclient.HandleResponse(response, "shield")
+	if err != nil {
+		return
+	}
+	var Json ResponseBody
+	err = json.Unmarshal(responseData, &Json)
+	if err != nil {
+		return
+	}
+	sInfo := Json.Data.Configs
 	for _, info := range sInfo {
 		shields = append(shields, info.ShieldName)
 	}
