385ed6b403db68faa748719a2ebe0fdb40697d9a
Matt Schiller <mschiller@banyansecurity.io>
2022-11-03 14:08:29 -0700

    refactor the connector resource

--- banyan/resource_connector_test.go
+++ banyan/resource_connector_test.go
@@ -3,7 +3,6 @@ package banyan
 import (
 	"fmt"
 	"github.com/banyansecurity/terraform-banyan-provider/client/satellite"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/stretchr/testify/assert"
@@ -11,32 +10,53 @@ import (
 )
 
 // Use the terraform plugin sdk testing framework for example testing connector lifecycle
-func TestAccConnector_basic(t *testing.T) {
-	var bnnConnector satellite.SatelliteTunnelConfig
-
-	rName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
-
-	resource.Test(t, resource.TestCase{
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckConnectorDestroy(t, "banyan_connector.example"),
-		Steps: []resource.TestStep{
-			// Creates the connector with the given terraform configuration and asserts that the connector is created
-			{
-				Config: testAccConnector_basic_create(rName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckExistingConnector("banyan_connector.example", &bnnConnector),
-					resource.TestCheckResourceAttr("banyan_connector.example", "name", rName),
-					resource.TestCheckResourceAttrPtr("banyan_connector.example", "id", &bnnConnector.ID),
-				),
-			},
-		},
-	})
-}
+//func TestAccConnector_basic(t *testing.T) {
+//	var bnnConnector satellite.SatelliteTunnelConfig
+//
+//	rName := fmt.Sprintf("tf-acc-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+//
+//	resource.Test(t, resource.TestCase{
+//		Providers:    testAccProviders,
+//		CheckDestroy: testAccCheckConnectorDestroy(t, "banyan_connector.example"),
+//		Steps: []resource.TestStep{
+//			// Creates the connector with the given terraform configuration and asserts that the connector is created
+//			{
+//				Config: testAccConnector_basic_create(rName),
+//				Check: resource.ComposeTestCheckFunc(
+//					testAccCheckExistingConnector("banyan_connector.example", &bnnConnector),
+//					resource.TestCheckResourceAttr("banyan_connector.example", "name", rName),
+//					resource.TestCheckResourceAttrPtr("banyan_connector.example", "id", &bnnConnector.ID),
+//				),
+//			},
+//		},
+//	})
+//}
+//
+//func TestAccConnector_tunnel(t *testing.T) {
+//	var bnnConnector satellite.SatelliteTunnelConfig
+//
+//	rName := fmt.Sprintf("tf-acc-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+//
+//	resource.Test(t, resource.TestCase{
+//		Providers:    testAccProviders,
+//		CheckDestroy: testAccCheckConnectorDestroy(t, "banyan_connector.example"),
+//		Steps: []resource.TestStep{
+//			// Creates the connector with the given terraform configuration and asserts that the connector is created
+//			{
+//				Config: testAccConnector_tunnel_create(rName),
+//				Check: resource.ComposeTestCheckFunc(
+//					testAccCheckExistingConnector("banyan_connector.example", &bnnConnector),
+//					resource.TestCheckResourceAttr("banyan_connector.example", "name", rName),
+//					resource.TestCheckResourceAttrPtr("banyan_connector.example", "id", &bnnConnector.ID),
+//				),
+//			},
+//		},
+//	})
+//}
 
 // Checks that the resource with the name resourceName exists and returns the connector object from the Banyan API
 func testAccCheckExistingConnector(resourceName string, bnnConnector *satellite.SatelliteTunnelConfig) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-
 		rs, ok := s.RootModule().Resources[resourceName]
 		if !ok {
 			return fmt.Errorf("resource not found in state %q", rs)
@@ -67,7 +87,7 @@ func testAccCheckConnectorDestroy(t *testing.T, resourceName string) resource.Te
 	}
 }
 
-// Returns terraform configuration for the connector. Takes in custom name.
+// Create a connector using terraform code and only the required parameters
 func testAccConnector_basic_create(name string) string {
 	return fmt.Sprintf(`
 resource "banyan_api_key" "example" {
@@ -78,7 +98,28 @@ resource "banyan_api_key" "example" {
 
 resource "banyan_connector" "example" {
   name              = "%s"
-  satellite_api_key_id = resource.banyan_api_key.example.id
+  api_key = resource.banyan_api_key.example.id
+  access_tiers = ["example"]
+  domains = ["example.com"]
+}
+`, name, name)
+}
+
+// Create a connector with a tunnel using terraform code
+func testAccConnector_tunnel_create(name string) string {
+	return fmt.Sprintf(`
+resource "banyan_api_key" "example" {
+  name              = "%s"
+  description       = "realdescription"
+  scope             = "satellite"
+}
+
+resource "banyan_connector" "example" {
+  name              = "%s"
+  api_key = resource.banyan_api_key.example.id
+  access_tiers = ["global-edge"]
+  cidrs = ["10.5.0.1/24"]
+  domains = ["example.com"]
 }
 `, name, name)
 }
