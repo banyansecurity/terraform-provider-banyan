31f31267864e9b5201cd70a6be852a41062c6181
Matt Schiller <mschiller@banyansecurity.io>
2022-11-03 13:44:17 -0700

    refactor rest client

--- client/client_holder.go
+++ client/client_holder.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"github.com/banyansecurity/terraform-banyan-provider/client/accesstier"
 	admin "github.com/banyansecurity/terraform-banyan-provider/client/admin"
 	"github.com/banyansecurity/terraform-banyan-provider/client/apikey"
 	"github.com/banyansecurity/terraform-banyan-provider/client/policy"
@@ -9,17 +10,22 @@ import (
 	"github.com/banyansecurity/terraform-banyan-provider/client/role"
 	"github.com/banyansecurity/terraform-banyan-provider/client/satellite"
 	service "github.com/banyansecurity/terraform-banyan-provider/client/service"
+	"github.com/banyansecurity/terraform-banyan-provider/client/shield"
 	"log"
+	"os"
 )
 
 type Holder struct {
-	Service          service.ServiceClienter
-	Policy           policy.PolicyClienter
-	Role             role.RoleClienter
+	Service          service.Client
+	Policy           policy.Client
+	Role             role.Client
 	PolicyAttachment policyattachment.Clienter
-	Admin            *admin.Admin
+	Admin            admin.Admin
 	Satellite        satellite.Clienter
 	ApiKey           apikey.Clienter
+	AccessTier       accesstier.Client
+	Shield           shield.Clienter
+	RestClient       *restclient.RestClient
 }
 
 // NewClientHolder returns a new client which is used to perform CRUD operations on all Banyan resources.
@@ -28,16 +34,25 @@ func NewClientHolder(hostUrl string, refreshToken string, apiToken string) (clie
 	if err != nil {
 		log.Fatalf("could not create client %s", err)
 	}
-	client2 := Holder{}
-	client = &client2
-	service := service.NewClient(restClient)
-	client.Service = service
-	client.Policy = policy.NewClient(restClient)
-	client.Role = role.NewClient(restClient)
-	client.PolicyAttachment = policyattachment.NewClient(restClient)
-	client.Satellite = satellite.NewClient(restClient)
-	client.ApiKey = apikey.NewClient(restClient)
-	admin := admin.NewClient(restClient)
-	client.Admin = admin
+	c := Holder{
+		Service:          service.NewClient(restClient),
+		Policy:           policy.NewClient(restClient),
+		Role:             role.NewClient(restClient),
+		PolicyAttachment: policyattachment.NewClient(restClient),
+		Satellite:        satellite.NewClient(restClient),
+		ApiKey:           apikey.NewClient(restClient),
+		AccessTier:       accesstier.NewClient(restClient),
+		Admin:            admin.NewClient(restClient),
+		Shield:           shield.NewClient(restClient),
+		RestClient:       restClient,
+	}
+	return &c, err
+}
+
+func GetClientHolderForTest() (newClient *Holder, err error) {
+	newClient, err = NewClientHolder(os.Getenv("BANYAN_HOST"), "", os.Getenv("BANYAN_API_KEY"))
+	if err != nil {
+		log.Fatal("Could not create the test client")
+	}
 	return
 }
--- client/restclient/restclient.go
+++ client/restclient/restclient.go
@@ -1,11 +1,14 @@
 package restclient
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -18,14 +21,18 @@ type RestClient struct {
 	httpClient  *http.Client
 }
 
-const defaultHostUrl = "http://net.banyanops.com"
+const defaultHostUrl = "https://net.banyanops.com"
 
 // New creates a new client that will let the user interact with the REST API server.
-// As part of this it exchanges the given refreshtoken for an acesstoken.
+// As part of this it exchanges the given refreshtoken or api key
 func New(hostUrl string, refreshToken string, apiToken string) (client *RestClient, err error) {
 	clientHostUrl := defaultHostUrl
 	if hostUrl != "" {
 		clientHostUrl = hostUrl
+	}
+
+	if !strings.HasSuffix(hostUrl, "/") {
+		clientHostUrl = hostUrl + "/"
 	}
 
 	var accessToken string
@@ -57,81 +64,81 @@ func New(hostUrl string, refreshToken string, apiToken string) (client *RestClie
 }
 
 // DoPut posts a message to host url, with the method path and body listed
-func (this *RestClient) DoPut(path string, body io.Reader) (response *http.Response, err error) {
-	req, err := this.NewRequest(http.MethodPut, path, body)
+func (c *RestClient) DoPut(path string, body io.Reader) (response *http.Response, err error) {
+	req, err := c.NewRequest(http.MethodPut, path, body)
 	if err != nil {
 		return
 	}
-	response, err = this.Do(req)
+	response, err = c.Do(req)
 	return
 }
 
 // DoPost posts a message to host url, with the method path and body listed
-func (this *RestClient) DoPost(path string, body io.Reader) (response *http.Response, err error) {
-	req, err := this.NewRequest(http.MethodPost, path, body)
+func (c *RestClient) DoPost(path string, body io.Reader) (response *http.Response, err error) {
+	req, err := c.NewRequest(http.MethodPost, path, body)
 	if err != nil {
 		return
 	}
-	response, err = this.Do(req)
+	response, err = c.Do(req)
 	return
 }
 
 // DoGet sends and does the get request
-func (this *RestClient) DoGet(path string) (response *http.Response, err error) {
-	request, err := this.Get(path)
+func (c *RestClient) DoGet(path string) (response *http.Response, err error) {
+	request, err := c.Get(path)
 	if err != nil {
 		return
 	}
-	response, err = this.Do(request)
+	response, err = c.Do(request)
 	return
 }
 
 // DoDelete sends the delete request
-func (this *RestClient) DoDelete(path string) (response *http.Response, err error) {
-	request, err := this.delete(path)
+func (c *RestClient) DoDelete(path string) (response *http.Response, err error) {
+	request, err := c.delete(path)
 	if err != nil {
 		return
 	}
-	response, err = this.Do(request)
+	response, err = c.Do(request)
 	return
 }
 
-func (this *RestClient) delete(path string) (request *http.Request, err error) {
-	return this.NewRequest(http.MethodDelete, path, nil)
+func (c *RestClient) delete(path string) (request *http.Request, err error) {
+	return c.NewRequest(http.MethodDelete, path, nil)
 }
 
 // Get creates a new Get request, saving the user from needing to pass in a nil value
-func (this *RestClient) Get(path string) (request *http.Request, err error) {
-	return this.NewRequest("GET", path, nil)
+func (c *RestClient) Get(path string) (request *http.Request, err error) {
+	return c.NewRequest("GET", path, nil)
 }
 
-func (this *RestClient) get(url string) (request *http.Request, err error) {
-	return this.newRequest("GET", url, nil)
+func (c *RestClient) get(url string) (request *http.Request, err error) {
+	return c.newRequest("GET", url, nil)
 }
 
 // Do executes the request and returns the response
-func (this *RestClient) Do(request *http.Request) (response *http.Response, err error) {
-	return this.httpClient.Do(request)
+func (c *RestClient) Do(request *http.Request) (response *http.Response, err error) {
+	return c.httpClient.Do(request)
 }
 
 // NewRequest creates a new request with the accessToken added as a header
-func (this *RestClient) NewRequest(method string, path string, body io.Reader) (request *http.Request, err error) {
-	return this.newRequest(method, this.hostUrl+path, body)
+func (c *RestClient) NewRequest(method string, path string, body io.Reader) (request *http.Request, err error) {
+	return c.newRequest(method, c.hostUrl+path, body)
 }
 
-func (this *RestClient) newRequest(method string, url string, body io.Reader) (request *http.Request, err error) {
+func (c *RestClient) newRequest(method string, url string, body io.Reader) (request *http.Request, err error) {
 	request, err = http.NewRequest(method, url, body)
 	if err != nil {
 		return
 	}
-	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", this.accessToken))
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.accessToken))
 	return
 }
 
-func (this *RestClient) exhangeRefreshTokenForAccessToken(clientHostUrl string, refreshToken string) (accessToken string, err error) {
+func (c *RestClient) exhangeRefreshTokenForAccessToken(clientHostUrl string, refreshToken string) (accessToken string, err error) {
 	req, err := http.NewRequest("POST", clientHostUrl+"api/v1/refresh_token", nil)
 	req.Header.Add("Authorization", "Bearer "+refreshToken)
-	resp, err := this.httpClient.Do(req)
+	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		err = errors.WithMessage(err, "Unable to make actual request for accesstoken")
 		return
@@ -161,5 +168,121 @@ func getAccessTokenFromJSON(body []byte) (accessToken string, err error) {
 	}
 	accessToken = accessTokenStruct.Message
 
+	return
+}
+
+type ErrorResponse struct {
+	Message string `json:"error_description"`
+}
+
+func (c *RestClient) Read(api string, component string, id string, path string) (resp []byte, err error) {
+	if id == "" {
+		err = fmt.Errorf("need an id to get %s", component)
+		return
+	}
+	if path == "" {
+		path = fmt.Sprintf("%s/%s/%s", api, component, id)
+	}
+	myUrl, err := url.Parse(path)
+	if err != nil {
+		return
+	}
+	response, err := c.DoGet(myUrl.String())
+	return HandleResponse(response, component)
+}
+
+func (c *RestClient) ReadQuery(component string, query url.Values, path string) (r []byte, err error) {
+	myUrl, err := url.Parse(path)
+	if err != nil {
+		return
+	}
+	myUrl.RawQuery = query.Encode()
+	response, err := c.DoGet(myUrl.String())
+	if err != nil {
+		return
+	}
+	return HandleResponse(response, component)
+}
+
+func (c *RestClient) Create(api string, component string, body []byte, path string) (r []byte, err error) {
+	if path == "" {
+		path = fmt.Sprintf("%s/%s", api, component)
+	}
+	request, err := c.NewRequest(http.MethodPost, path, bytes.NewBuffer(body))
+	if err != nil {
+		return
+	}
+	response, err := c.Do(request)
+	if err != nil {
+		return
+	}
+	return HandleResponse(response, component)
+}
+
+func (c *RestClient) Update(api string, component string, id string, body []byte, path string) (r []byte, err error) {
+	if path == "" {
+		path = fmt.Sprintf("%s/%s/%s", api, component, id)
+	}
+	request, err := c.NewRequest(http.MethodPut, path, bytes.NewBuffer(body))
+	if err != nil {
+		return
+	}
+	response, err := c.Do(request)
+	return HandleResponse(response, component)
+}
+
+func (c *RestClient) Delete(api string, component string, id string, path string) (err error) {
+	if id == "" {
+		err = fmt.Errorf("need an id to delete %s", component)
+		return
+	}
+	if path == "" {
+		path = fmt.Sprintf("%s/%s/%s", api, component, id)
+	}
+	myUrl, err := url.Parse(path)
+	if err != nil {
+		return
+	}
+	response, err := c.DoDelete(myUrl.String())
+	_, err = HandleResponse(response, component)
+	return
+}
+
+func (c *RestClient) DeleteQuery(component string, id string, query url.Values, path string) (err error) {
+	if id == "" {
+		err = fmt.Errorf("need an id to delete %s", component)
+		return
+	}
+	myUrl, err := url.Parse(path)
+	if err != nil {
+		return
+	}
+	myUrl.RawQuery = query.Encode()
+	response, err := c.DoDelete(myUrl.String())
+	_, err = HandleResponse(response, component)
+	return
+}
+
+func HandleResponse(response *http.Response, component string) (responseData []byte, err error) {
+	defer response.Body.Close()
+	responseData, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	if response.StatusCode == 404 || response.StatusCode == 400 {
+		err = fmt.Errorf("%s not found", component)
+		return
+	}
+	if response.StatusCode != 200 {
+		var errResp ErrorResponse
+		if err != nil {
+			return
+		}
+		uerr := json.Unmarshal(responseData, &errResp)
+		if uerr == nil {
+			err = fmt.Errorf("received error code %d with message: %s", response.StatusCode, errResp.Message)
+		}
+		return
+	}
 	return
 }
