417d05468efda79b411f86043770aa76c1195b68
Matt Schiller <mschiller@banyansecurity.io>
2022-11-03 14:58:01 -0700

    update provider

--- banyan/provider.go
+++ banyan/provider.go
@@ -3,6 +3,7 @@ package banyan
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	bnnClient "github.com/banyansecurity/terraform-banyan-provider/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -16,7 +17,7 @@ func Provider() *schema.Provider {
 			"host": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("BANYAN_HOST", "https://console.banyanops.com"),
+				DefaultFunc: schema.EnvDefaultFunc("BANYAN_HOST", "https://net.banyanops.com/"),
 			},
 			"api_token": {
 				Type:       schema.TypeString,
@@ -53,6 +54,7 @@ func Provider() *schema.Provider {
 			"banyan_policy_attachment": resourcePolicyAttachment(),
 			"banyan_api_key":           resourceApiKey(),
 			"banyan_connector":         resourceConnector(),
+			"banyan_accesstier":        resourceAccessTier(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"banyan_oidc_settings": dataSourceOidcSettings(),
@@ -61,32 +63,28 @@ func Provider() *schema.Provider {
 	}
 }
 
+// Remove once fully depreciated
+func chooseApiKey(d *schema.ResourceData) (apikey string) {
+	k, ok := d.GetOk("api_key")
+	if !ok {
+		return d.Get("api_token").(string)
+	}
+	return k.(string)
+}
+
 // Configures the Banyan provider with the given refresh / API token and host url
 func providerConfigure(ctx context.Context, d *schema.ResourceData) (client interface{}, diagnostic diag.Diagnostics) {
-	refreshToken := d.Get("refresh_token").(string)
-	var domain string
-	unAssertedDomain := d.Get("host")
-	if unAssertedDomain == nil {
-		domain = "https://net.banyanops.com/"
-	} else {
-		domainTypeAsserted, ok := unAssertedDomain.(string)
-		if !ok {
-			diagnostic = append(diagnostic, diag.Diagnostic{
-				Severity: diag.Error,
-				Summary:  "Unable to create Banyan client",
-				Detail:   "Unable to authenticate against the provided banyan host url with the given API token",
-			})
-		}
-		domain = domainTypeAsserted
+	host := d.Get("host").(string)
+	if !strings.HasSuffix(host, "/") {
+		host = host + "/"
 	}
-	client, err := bnnClient.NewClientHolder(domain, refreshToken, d.Get("api_token").(string))
+	client, err := bnnClient.NewClientHolder(host, d.Get("refresh_token").(string), chooseApiKey(d))
 	if err != nil {
 		diagnostic = append(diagnostic, diag.Diagnostic{
 			Severity: diag.Error,
 			Summary:  "Unable to create Banyan client",
-			Detail:   "Unable to authenticate with the given API token" + fmt.Sprintf("%+v", err),
+			Detail:   "Unable to authenticate to the Banyan API" + fmt.Sprintf("%+v", err),
 		})
-		return
 	}
 	return
 }
