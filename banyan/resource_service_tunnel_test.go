df8c4ad54bb93ad4ae972d4248bee3e220ab99ae
Matt Schiller <mschiller@banyansecurity.io>
2022-11-09 04:25:28 -0800

    add service tunnel

---
+++ banyan/resource_service_tunnel_test.go
@@ -0,0 +1,81 @@
+package banyan
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+const apiKeyID = "f0da9734-10b7-4ace-85ae-05206119cc69"
+
+// Use the terraform plugin sdk testing framework for example testing servicetunnel lifecycle
+func TestAccServiceTunnel_basic(t *testing.T) {
+
+	rName := fmt.Sprintf("tf-acc-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			// Creates the servicetunnel with the given terraform configuration and asserts that the servicetunnel is created
+			{
+				Config: fmt.Sprintf(`
+					resource banyan_accesstier "example" {
+						name = "%s"
+						address = "*.example.com"
+						api_key_id = "%s"
+					}
+
+					resource "banyan_policy_infra" "example" {
+						name        = "%s"
+						description = "some infrastructure policy description"
+						access {
+							roles       = ["ANY"]
+							trust_level = "High"
+						}
+					}
+
+					resource "banyan_service_tunnel" "example" {
+						name              = "%s"
+						description       = "realdescription"
+						access_tier       = banyan_accesstier.example.name
+                        policy            = banyan_policy_infra.example.id
+					}
+					`, rName, apiKeyID, rName, rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("banyan_service_tunnel.example", "name", rName),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+					resource banyan_accesstier "example" {
+						name = "%s"
+						address = "*.example.com"
+						api_key_id = "%s"
+						tunnel_cidrs = ["10.0.0.1/24"]
+					}
+
+					resource "banyan_policy_infra" "example" {
+						name        = "%s"
+						description = "some infrastructure policy description"
+						access {
+							roles       = ["ANY"]
+							trust_level = "High"
+						}
+					}
+
+					resource "banyan_service_tunnel" "example" {
+						name              = "%s"
+						description       = "realdescription update"
+						access_tier       = banyan_accesstier.example.name
+                        policy            = banyan_policy_infra.example.id
+					}
+					`, rName, apiKeyID, rName, rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("banyan_service_tunnel.example", "name", rName),
+				),
+			},
+		},
+	})
+}
---
+++ client/servicetunnel/client.go
@@ -0,0 +1,144 @@
+package servicetunnel
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/banyansecurity/terraform-banyan-provider/client/restclient"
+	"html"
+)
+
+const apiVersion = "api/v2"
+const component = "service_tunnel"
+
+type ServiceTunnel struct {
+	restClient *restclient.RestClient
+}
+
+// NewClient returns a new client for interacting with the access tier resource
+func NewClient(restClient *restclient.RestClient) Client {
+	client := ServiceTunnel{
+		restClient: restClient,
+	}
+	return &client
+}
+
+type Client interface {
+	Get(id string) (spec ServiceTunnelInfo, err error)
+	Create(spec Info) (created ServiceTunnelInfo, err error)
+	Update(id string, spec Info) (updated ServiceTunnelInfo, err error)
+	Delete(id string) (err error)
+	AttachPolicy(id string, post PolicyAttachmentPost) (created PolicyAttachmentInfo, err error)
+	DeletePolicy(tunID string, policyID string) (err error)
+}
+
+func (a *ServiceTunnel) Get(id string) (spec ServiceTunnelInfo, err error) {
+	resp, err := a.restClient.Read(apiVersion, component, id, "")
+	if err != nil {
+		return
+	}
+	spec, err = specFromResponse(resp)
+	return
+}
+
+func (a *ServiceTunnel) Create(spec Info) (created ServiceTunnelInfo, err error) {
+	body, err := json.Marshal(Info{
+		Kind:       "BanyanAccessTier",
+		APIVersion: "rbac.banyanops.com/v1",
+		Type:       "attribute-based",
+		Metadata: Metadata{
+			Name:         spec.Metadata.Name,
+			FriendlyName: spec.Metadata.FriendlyName,
+			Description:  spec.Metadata.Description,
+		},
+		Spec: spec.Spec,
+	})
+	resp, err := a.restClient.Create(apiVersion, component, body, "")
+	if err != nil {
+		return
+	}
+	created, err = specFromResponse(resp)
+	return
+}
+
+func (a *ServiceTunnel) Update(id string, spec Info) (updated ServiceTunnelInfo, err error) {
+	body, err := json.Marshal(Info{
+		Kind:       "BanyanAccessTier",
+		APIVersion: "rbac.banyanops.com/v1",
+		Type:       "attribute-based",
+		Metadata: Metadata{
+			Name:         spec.Metadata.Name,
+			FriendlyName: spec.Metadata.FriendlyName,
+			Description:  spec.Metadata.Description,
+		},
+		Spec: spec.Spec,
+	})
+	resp, err := a.restClient.Update(apiVersion, component, id, body, "")
+	if err != nil {
+		return
+	}
+	updated, err = specFromResponse(resp)
+	return
+}
+
+func (a *ServiceTunnel) Delete(id string) (err error) {
+	err = a.restClient.Delete(apiVersion, component, id, "")
+	return
+}
+
+func (a *ServiceTunnel) GetPolicy(id string) (policy PolicyAttachmentInfo, err error) {
+	path := fmt.Sprintf("%s/%s/%s/security_policy", apiVersion, component, id)
+	resp, err := a.restClient.Read(apiVersion, component, id, path)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(resp, &policy)
+	return
+}
+
+func (a *ServiceTunnel) AttachPolicy(id string, post PolicyAttachmentPost) (created PolicyAttachmentInfo, err error) {
+	if id == "" {
+		err = fmt.Errorf("need service tunnel id to attach a policy")
+	}
+	body, err := json.Marshal(post)
+	path := fmt.Sprintf("%s/%s/%s/security_policy", apiVersion, component, id)
+	resp, err := a.restClient.Create(apiVersion, component, body, path)
+	if err != nil {
+		return
+	}
+	var j PolicyResponse
+	err = json.Unmarshal(resp, &j)
+	created = j.Data
+	return
+}
+
+func (a *ServiceTunnel) DeletePolicy(tunID string, policyID string) (err error) {
+	path := fmt.Sprintf("%s/%s/%s/security_policy/%s", apiVersion, component, tunID, policyID)
+	err = a.restClient.Delete(apiVersion, component, tunID, path)
+	return
+}
+
+func specFromResponse(respData []byte) (created ServiceTunnelInfo, err error) {
+	var j Response
+	err = json.Unmarshal(respData, &j)
+	if err != nil {
+		return
+	}
+	spec := j.Data
+	var jSpec Info
+	specString := html.UnescapeString(spec.Spec)
+	err = json.Unmarshal([]byte(specString), &jSpec)
+	created = ServiceTunnelInfo{
+		ID:           spec.ID,
+		OrgID:        spec.OrgID,
+		Name:         spec.Name,
+		FriendlyName: spec.FriendlyName,
+		Description:  spec.Description,
+		Enabled:      spec.Enabled,
+		Spec:         jSpec.Spec,
+		CreatedAt:    spec.CreatedAt,
+		CreatedBy:    spec.CreatedBy,
+		UpdatedAt:    spec.UpdatedAt,
+		UpdatedBy:    spec.UpdatedBy,
+	}
+	return
+}
