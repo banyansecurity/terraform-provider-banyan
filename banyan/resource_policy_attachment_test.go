42e1e6b8c3c2e357055d8876e03ccf370ffea4a8
Matt Schiller <mschiller@banyansecurity.io>
2022-11-08 16:37:16 -0800

    update the policy attachment to handle depreciations

--- banyan/resource_policy_attachment_test.go
+++ banyan/resource_policy_attachment_test.go
@@ -3,32 +3,37 @@ package banyan
 import (
 	"fmt"
 	"github.com/banyansecurity/terraform-banyan-provider/client/policyattachment"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-//// Use the terraform plugin sdk testing framework for acceptance testing policyattachment lifecycle
-//func TestAccPolicyAttachment_lifecycle(t *testing.T) {
-//
-//	rName := fmt.Sprintf("tf-acc-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
-//
-//	resource.Test(t, resource.TestCase{
-//		Providers: testAccProviders,
-//		Steps: []resource.TestStep{
-//			{
-//				Config: testAccPolicyAttachment_lifecycle_create(rName),
-//			},
-//			{
-//				Config: testAccPolicyAttachment_lifecycle_attach_multiple(rName),
-//			},
-//			{
-//				Config: testAccPolicyAttachment_lifecycle_detach(rName),
-//			},
-//		},
-//	})
-//}
+// Use the terraform plugin sdk testing framework for acceptance testing policyattachment lifecycle
+// This resource is depreciated and used with the depreciated sevice schemas. this tests the depreciated lifecycle
+// and will be removed in the future
+func TestAccPolicyAttachment_lifecycle(t *testing.T) {
+
+	var policyAttachment policyattachment.GetBody
+
+	rName := fmt.Sprintf("tf-acc-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPolicyAttachment_lifecycle_create(rName),
+				Check:  testAccCheckExistingPolicyAttachment("banyan_policy_attachment.example", &policyAttachment),
+			},
+			{
+				Config: testAccPolicyAttachment_lifecycle_attach_multiple(rName),
+			},
+			{
+				Config: testAccPolicyAttachment_lifecycle_detach(rName),
+			},
+		},
+	})
+}
 
 // Checks that the resource with the name resourceName exists and returns the policyattachment object from the Banyan API
 func testAccCheckExistingPolicyAttachment(resourceName string, policyAttachment *policyattachment.GetBody) resource.TestCheckFunc {
@@ -50,37 +55,12 @@ func testAccCheckExistingPolicyAttachment(resourceName string, policyAttachment 
 	}
 }
 
-// Asserts using the API that the groups for the policyattachment were updated
-func testAccCheckPolicyAttachmentUpdated(t *testing.T, bnnPolicyAttachment *policyattachment.GetBody, resourceName string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("resource not found %q", rs)
-		}
-
-		if bnnPolicyAttachment.AttachedToID != rs.Primary.Attributes["attached_to_id"] {
-			return fmt.Errorf("incorrect attachment_id, expected %s, got: %s", bnnPolicyAttachment.AttachedToID, rs.Primary.Attributes["attached_to_id"])
-		}
-		return nil
-	}
-}
-
-// Uses the API to check that the policyattachment was destroyed
-func testAccCheckPolicyAttachmentDestroy(t *testing.T, policyAttachment *policyattachment.GetBody) resource.TestCheckFunc {
-	emptyPolicyAttachment := policyattachment.GetBody{}
-	return func(s *terraform.State) error {
-		r, err := testAccClient.PolicyAttachment.Get(policyAttachment.AttachedToID, policyAttachment.AttachedToType)
-		assert.Equal(t, r, emptyPolicyAttachment)
-		return err
-	}
-}
-
 func testAccPolicyAttachment_lifecycle_create(name string) string {
 	return fmt.Sprintf(`
 resource "banyan_service_infra_tcp" "acctest-policy-attachment-lifecycle" {
   name        = "%s"
   description = "some tcp service description"
-  cluster     = "us-west"
+  cluster     = "tortoise"
   access_tier   = "us-west1"
   domain =  "%s.corp.com"
   backend_domain = "%s.internal"
@@ -102,7 +82,7 @@ resource "banyan_role" "everyone" {
   user_group = ["Everyone"]
 }
 
-resource "banyan_policy_attachment" "acctest-policy-attachment-lifecycle" {
+resource "banyan_policy_attachment" "example" {
   policy_id        = banyan_policy_infra.high-trust-any.id
   attached_to_type = "service"
   attached_to_id   = banyan_service_infra_tcp.acctest-policy-attachment-lifecycle.id
@@ -115,7 +95,7 @@ func testAccPolicyAttachment_lifecycle_attach_multiple(name string) string {
 resource "banyan_service_infra_tcp" "acctest-policy-attachment-lifecycle" {
   name        = "%s"
   description = "some tcp service description"
-  cluster     = "us-west"
+  cluster     = "tortoise"
   access_tier   = "us-west1"
   domain =  "%s.corp.com"
   backend_domain = "%s.internal"
@@ -125,7 +105,7 @@ resource "banyan_service_infra_tcp" "acctest-policy-attachment-lifecycle" {
 resource "banyan_service_infra_tcp" "acctest-policy-attachment-lifecycle-two" {
   name        = "%s-two"
   description = "some tcp service description"
-  cluster     = "us-west"
+  cluster     = "tortoise"
   access_tier   = "us-west1"
   domain =  "%s-two.corp.com"
   backend_domain = "%s-two.internal"
@@ -168,7 +148,7 @@ func testAccPolicyAttachment_lifecycle_detach(name string) string {
 resource "banyan_service_infra_tcp" "acctest-policy-attachment-lifecycle" {
   name        = "%s"
   description = "some tcp service description"
-  cluster     = "us-west"
+  cluster     = "tortoise"
   access_tier   = "us-west1"
   domain =  "%s.corp.com"
   backend_domain = "%s.internal"
