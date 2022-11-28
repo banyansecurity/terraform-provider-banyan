daed399dd785b6936bf6a786841fdd200629de2c
Matt Schiller <mschiller@banyansecurity.io>
2022-11-28 08:59:26 -0800

    update policy tunnel example

--- examples/resources/banyan_policy_tunnel/resource.tf
+++ examples/resources/banyan_policy_tunnel/resource.tf
@@ -1,19 +1,19 @@
 resource "banyan_policy_tunnel" "example" {
-  name        = "%s"
+  name        = "example"
   description = "some tunnel policy description"
   access {
     roles       = ["Everyone"]
     trust_level = "Low"
     l4_access {
       allow {
-        cidrs = ["10.10.10.0/24"]
-        protocols = ["UDP"]
-        ports = ["80"]
+        cidrs     = ["10.10.10.0/24"]
+        protocols = ["TCP"]
+        ports     = ["443"]
       }
       deny {
-        cidrs = ["10.10.11.0/24"]
+        cidrs     = ["10.10.10.0/24"]
         protocols = ["TCP"]
-        ports = ["80"]
+        ports     = ["80"]
       }
     }
   }
