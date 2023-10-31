05883b79db6223b9aee2be8f05b2ac5b837d8b6d
Naresh Kakubal <naresh@banyansecurity.io>
2023-10-31 12:05:44 -0700

    incorrect document usage fix for data block

--- examples/data-sources/banyan_role/data-source.tf
+++ examples/data-sources/banyan_role/data-source.tf
@@ -1,18 +1,10 @@
 data "banyan_role" "example" {
     name = "my-example-policy"
-    description = "realdescription"
-    container_fqdn = ["asdf.asdf"]
-    known_device_only = true
-    platform = ["macOS", "Android"]
-    user_group = ["group1", "group2"]
-    email = ["john@marsha.com"]
-    device_ownership = ["Corporate Dedicated", "Employee Owned"]
-    mdm_present = true
-   serial_numbers = ["DeviceSerial1"]
 }
 
 resource "banyan_service_web" "example" {
   name = "example-infra-service"
   policy = data.banyan_policy_infra.example.id
-  
+  backend_domain = "backend-domain.com"
+  domain         = "my-domain.com"
 }
