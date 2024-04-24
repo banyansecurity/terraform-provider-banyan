a28f34d9ee300fc8a126c265cad221fc44359d9e
rohitSangamnerkar <148537411+rohitSangamnerkar@users.noreply.github.com>
2024-04-24 20:26:55 +0530

    one click access for rdp changes (#168)
    * one click access for rdp changes

--- banyan/resource_service_rdp_test.go
+++ banyan/resource_service_rdp_test.go
@@ -22,6 +22,7 @@ func TestSchemaServiceInfraRdp_rdp_conn(t *testing.T) {
 		"backend_domain":                 "10.10.2.1",
 		"backend_port":                   3309,
 		"client_banyanproxy_listen_port": 9109,
+		"rdp_settings":                   []interface{}{"devicestoredirect:s:*"},
 	}
 
 	d := schema.TestResourceDataRaw(t, RdpSchema(), svc_rdp_conn)
@@ -43,6 +44,7 @@ func TestSchemaServiceInfraRdp_rdp_collection(t *testing.T) {
 		"domain":                         "test-rdp-collection.tdupnsan.getbnn.com",
 		"http_connect":                   true,
 		"client_banyanproxy_listen_port": 9108,
+		"rdp_settings":                   []interface{}{"devicestoredirect:s:*"},
 	}
 
 	d := schema.TestResourceDataRaw(t, RdpSchema(), svc_rdp_collection)
@@ -78,6 +80,29 @@ func TestAccService_infra_rdp(t *testing.T) {
 	})
 }
 
+func TestAccService_infra_rdp_without_rdp_settings(t *testing.T) {
+	var bnnService service.GetServiceSpec
+	rName := fmt.Sprintf("tf-acc-%s", acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum))
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckServiceDestroy(t, &bnnService.ServiceID),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccService_infra_rdp_create_without_rdp_settings(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckExistingService("banyan_service_rdp.example_without_rdp", &bnnService),
+					testAccCheckServiceAgainstJson(t, testAccService_infra_rdp_create_without_rdp_settings_json(rName), &bnnService.ServiceID),
+				),
+			},
+			{
+				ResourceName:      "banyan_service_rdp.example_without_rdp",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 // Returns terraform configuration for a typical rdp service
 func testAccService_infra_rdp_create(name string) string {
 	return fmt.Sprintf(`
@@ -88,11 +113,141 @@ resource "banyan_service_rdp" "example" {
   domain         = "%s-rdp.corp.com"
   backend_domain = "%s-rdp.internal"
   backend_port   = 3389
+  rdp_settings   = ["devicestoredirect:s:*"]
 }
 `, name, name, name)
 }
 
 func testAccService_infra_rdp_create_json(name string) string {
+	return fmt.Sprintf(`
+{
+    "kind": "BanyanService",
+    "apiVersion": "rbac.banyanops.com/v1",
+    "type": "origin",
+    "metadata": {
+        "name": "%s-rdp",
+        "description": "some RDP service description",
+        "cluster": "cluster1",
+        "tags": {
+            "template": "TCP_USER",
+            "user_facing": "true",
+            "protocol": "tcp",
+            "domain": "%s-rdp.corp.com",
+            "port": "8443",
+            "icon": "",
+            "service_app_type": "RDP",
+            "banyanproxy_mode": "TCP",
+            "app_listen_port": "",
+            "allow_user_override": true,
+            "description_link": "",
+            "rdp_settings": ["devicestoredirect:s:*"]
+        }
+    },
+    "spec": {
+        "attributes": {
+            "tls_sni": [
+                "%s-rdp.corp.com"
+            ],
+            "frontend_addresses": [
+                {
+                    "cidr": "",
+                    "port": "8443"
+                }
+            ],
+            "host_tag_selector": [
+                {
+                    "com.banyanops.hosttag.site_name": "us-west1"
+                }
+            ],
+            "disable_private_dns": false
+        },
+        "backend": {
+            "target": {
+                "name": "%s-rdp.internal",
+                "port": "3389",
+                "tls": false,
+                "tls_insecure": false,
+                "client_certificate": false
+            },
+            "dns_overrides": {},
+            "whitelist": [],
+            "connector_name": ""
+        },
+        "cert_settings": {
+            "dns_names": [
+                "%s-rdp.corp.com"
+            ],
+            "custom_tls_cert": {
+                "enabled": false,
+                "cert_file": "",
+                "key_file": ""
+            },
+            "letsencrypt": false
+        },
+        "http_settings": {
+            "enabled": false,
+            "oidc_settings": {
+                "enabled": false,
+                "service_domain_name": "",
+                "post_auth_redirect_path": "",
+                "api_path": "",
+                "trust_callbacks": null,
+                "suppress_device_trust_verification": false
+            },
+            "http_health_check": {
+                "enabled": false,
+                "addresses": null,
+                "method": "",
+                "path": "",
+                "user_agent": "",
+                "from_address": [],
+                "https": false
+            },
+            "http_redirect": {
+                "enabled": false,
+                "addresses": null,
+                "from_address": null,
+                "url": "",
+                "status_code": 0
+            },
+            "exempted_paths": {
+                "enabled": false,
+                "patterns": [
+                    {
+                        "hosts": [
+                            {
+                                "origin_header": [],
+                                "target": []
+                            }
+                        ],
+                        "methods": [],
+                        "paths": [],
+                        "mandatory_headers": []
+                    }
+                ]
+            },
+            "headers": {}
+        },
+        "client_cidrs": []
+    }
+}
+`, name, name, name, name, name)
+}
+
+func testAccService_infra_rdp_create_without_rdp_settings(name string) string {
+	return fmt.Sprintf(`
+resource "banyan_service_rdp" "example_without_rdp" {
+  name           = "%s-rdp"
+  description    = "some RDP service description"
+  access_tier    = "us-west1"
+  domain         = "%s-rdp.corp.com"
+  backend_domain = "%s-rdp.internal"
+  backend_port   = 3389
+}
+`, name, name, name)
+}
+
+func testAccService_infra_rdp_create_without_rdp_settings_json(name string) string {
 	return fmt.Sprintf(`
 {
     "kind": "BanyanService",
@@ -200,7 +355,7 @@ func testAccService_infra_rdp_create_json(name string) string {
                 ]
             },
             "headers": {}
-        },        
+        },
         "client_cidrs": []
     }
 }
