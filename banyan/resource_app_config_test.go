3cfec3017e30cf37d0faf80cb4edabe7e289a07f
Sparsh Arora <sparsh.arora@joshsoftware.com>
2024-06-03 17:54:37 +0530

    test code update

--- banyan/resource_app_config_test.go
+++ banyan/resource_app_config_test.go
@@ -1,55 +1,32 @@
 package banyan
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
+	"os/exec"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-var existingID string
-
-// Use the terraform plugin sdk testing framework for example testing apikey lifecycle
-// func TestAccAppConfig_basic(t *testing.T) {
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck: func() {
-// 			testAccCheckBanyanConfigDestroy(t)
-// 		},
-// 		Providers:    testAccProviders,
-// 		CheckDestroy: nil,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccAppConfig_basic_create(),
-// 				Check:  resource.ComposeTestCheckFunc(),
-// 			},
-// 		},
-// 	})
-// }
+var existingResourceID string
 
 func TestAccAppConfig_update(t *testing.T) {
-
-	var existingResourceID string
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
-			testAccCheckBanyanConfigExists("banyan_app_config.example", &existingResourceID)
+			fmt.Println("Executing PreConfig...")
+			existingResourceID = testAccCheckBanyanConfig()
 		},
 		Providers:    testAccProviders,
 		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
-				// PreConfig: func() {
-				// 	existingResourceID, err := checkExistingResourceID()
-				// 	if err != nil {
-				// 		t.Fatal(err)
-				// 	}
-				// 	if existingResourceID != "" {
-				// 		t.Skip("Resource already exists, skipping creation step")
-				// 	}
-				// },
-				// PreConfig: func() { testAccCheckBanyanConfigDestroy(t) },
-				Config:    testAccAppConfig_conditionalCreate(existingResourceID),
+				PreConfig: func() {
+					fmt.Println("Executing PreConfig...")
+					existingResourceID = testAccCheckBanyanConfig()
+				},
+				Config: testAccAppConfig_conditionalCreate(existingResourceID),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					func(s *terraform.State) error {
 						if existingResourceID == "" {
@@ -71,6 +48,7 @@ func TestAccAppConfig_update(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 				ImportStateIdFunc: func(*terraform.State) (string, error) {
+					fmt.Println("ImportStateIdFunc called")
 					return existingResourceID, nil
 				},
 			},
@@ -107,8 +85,31 @@ func testAccAppConfig_conditionalCreate(existingResourceID string) string {
 	return ``
 }
 
-func testAccPreCheckResourceExists(resourceName string, existingResourceID *string) error {
-	state := terraform.NewState()
+func testAccCheckBanyanConfig() string {
+	fmt.Println("Executing function...")
+	client := NewAccClient()
+
+	present, err := client.AppConfig.Get("random")
+	if err != nil {
+		fmt.Println("Error checking nrpt_config presence: ", err)
+		return ""
+	}
+
+	return present.Data.ID
+}
+
+func testAccCheckBanyanConfigExists(resourceName string, existingResourceID *string) error {
+	cmd := exec.Command("terraform", "state", "pull")
+	output, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("error running terraform state pull: %v", err)
+	}
+
+	var state terraform.State
+	if err := json.Unmarshal(output, &state); err != nil {
+		return fmt.Errorf("error unmarshalling state: %v", err)
+	}
+
 	rs, ok := state.RootModule().Resources[resourceName]
 	if !ok {
 		// Resource not found in state
@@ -118,64 +119,5 @@ func testAccPreCheckResourceExists(resourceName string, existingResourceID *stri
 	if rs.Primary.ID != "" {
 		*existingResourceID = rs.Primary.ID
 	}
-	return nil
-}
-
-// func checkExistingResourceID(provider *schema.Provider) (string, error) {
-// 	// Read the Terraform state using the provider
-// 	state := provider.Meta().(*ProviderMeta).RawState
-
-// 	// Get the resource instance
-// 	rs, ok := state.RootModule().Resources["banyan_app_config.example"]
-// 	if !ok {
-// 		return "", fmt.Errorf("Resource not found: banyan_app_config.example")
-// 	}
-
-// 	// Check if the resource has an ID
-// 	if rs.Primary.ID == "" {
-// 		return "", fmt.Errorf("No ID is set for the resource: banyan_app_config.example")
-// 	}
-
-// 	return rs.Primary.ID, nil
-// }
-
-func testAccCheckBanyanConfig() string {
-	client := NewAccClient()
-
-	present, err := client.AppConfig.Get("sad")
-	if err != nil {
-		return ""
-	}
-
-	log.Fatalf("ID %s", present.Data.ID)
-	return present.Data.ID
-}
-func testAccCheckBanyanConfigExists(resourceName string, existingID *string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("Not found: %s", resourceName)
-		}
-
-		if rs.Primary.ID != "" {
-			*existingID = rs.Primary.ID
-		}
-
-		return nil
-	}
-}
-
-func testAccCheckBanyanConfigDestroy(t *testing.T) error {
-	client := NewAccClient()
-
-	present, err := client.AppConfig.Get("sad")
-	if err != nil {
-		return fmt.Errorf("Error checking nrpt_config presence: %s", err)
-	}
-
-	if present.Data.ID != "" {
-		t.Skip("skipping")
-	}
-
 	return nil
 }
